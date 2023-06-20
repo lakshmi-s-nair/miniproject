70480ce88bb3c922a8408d89ce6057fd5e59aa8a
Lakshmi S Nair <83352292+lakshmi-s-nair@users.noreply.github.com>
2023-06-20 10:10:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_restful import Resource, Api, reqparse
 from Fetch_images import storage, database
 import sys
 import numpy as np
@@ -21,6 +22,7 @@ with open('SVC_model.pkl', 'rb') as f:
 
 
 app = Flask(__name__)
+api = Api(app)
 data = {
        'Beauty & Hygiene' : 0,
        'Kitchen, Garden & Pets': 0, 
